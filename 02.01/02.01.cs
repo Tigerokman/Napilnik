32aa621e21389cae233770698a2f6a1e12add060
Tigerokman <mr.lazaridi@mail.ru>
2022-08-17 16:58:40 +0300

    Update 02.01.cs

--- 02.01/02.01.cs
+++ 02.01/02.01.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.IO;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class ConsoleLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);0
+        }
+    }
+
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+}
